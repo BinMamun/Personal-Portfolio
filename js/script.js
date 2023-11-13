13759d55f72126a5260a6b620fd685018355d7aa
Abdullah Bin Mamun <binbean44@gmail.com>
2023-11-13 22:16:11 +0600

    profile-pic & profile-info design

--- js/script.js
+++ js/script.js
@@ -5,3 +5,20 @@ function toggleMenu() {
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+
+// download the cv button
+document.querySelector(".js-download-cv")
+    .addEventListener("click", () => {
+        window.open('./assets/resume-example.pdf');
+    });
+
+//leads the user to the contact part of the website
+document.querySelector(".js-contact-info")
+    .addEventListener("click", () => {
+        location.href = "#contact";
+    });
+
+document.querySelector(".js-github-profile")
+    .addEventListener("click", () => {
+        location.href = "https://github.com/BinMamun";
+    })
