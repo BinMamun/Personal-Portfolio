4f5bde446907e979fe9365ffefea1703f2616fcc
Abdullah Bin Mamun <binbean44@gmail.com>
2023-11-14 03:24:17 +0600

    js documentation

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+//toggles the hamburger menu
 function toggleMenu() {
     const menu = document.querySelector(".menu-links");
     const icon = document.querySelector(".hamburger-icon");
@@ -6,7 +7,7 @@ function toggleMenu() {
     icon.classList.toggle("open");
 }
 
-// download the cv button
+// download cv button
 document.querySelector(".js-download-cv")
     .addEventListener("click", () => {
         window.open('./assets/Abdullah Bin Mamun.pdf');
@@ -18,11 +19,14 @@ document.querySelector(".js-contact-info")
         location.href = "#contact";
     });
 
+
+//redirects the user to my github account
 document.querySelector(".js-github-profile")
     .addEventListener("click", () => {
         location.href = "https://github.com/BinMamun";
     });
 
+//leads the user to the experience part of the website
 document.querySelector(".js-arrow-to-experience")
     .addEventListener("click", () => {
         location.href = "./#experience";
@@ -32,6 +36,8 @@ document.querySelector(".js-arrow-to-projects")
     .addEventListener("click", () => {
         location.href = "./#projects";
     });
+
+//leads the user to the contact part of the website
 document.querySelector(".js-arrow-to-contact")
     .addEventListener("click", () => {
         location.href = "./#contact";
